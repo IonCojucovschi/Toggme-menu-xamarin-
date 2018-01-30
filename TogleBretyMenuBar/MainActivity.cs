70c1e66e68e12e84dbe47abfdc2e9cf791c5ce18
admin_pk\Admin <Cojucovschi@gmail.com>
2018-01-30 16:10:35 +0200

    first commit togle menu bar

---
+++ TogleBretyMenuBar/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using SupportToolbar = Android.Support.V7.Widget.Toolbar;
+namespace TogleBretyMenuBar
+{
+    [Activity(Label = "TogleBretyMenuBar", MainLauncher = true,Theme ="@style/MyTheme")]
+    public class MainActivity : AppCompatActivity
+    {
+        private SupportToolbar mTolbar;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            mTolbar = FindViewById<SupportToolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(mTolbar);
+        }
+    }
+}
+
---
+++ TogleBretyMenuBar/MyActionBarDrawerToggle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V4.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using SupportActionBarDrawerToggle = Android.Support.V7.App.ActionBarDrawerToggle;
+
+namespace TogleBretyMenuBar
+{
+   public  class MyActionBarDrawerToggle:SupportActionBarDrawerToggle
+    {
+        private AppCompatActivity mHostActivity;
+        private int mOpenedResource;
+        private int mClosedResource;
+
+        public MyActionBarDrawerToggle(AppCompatActivity host,DrawerLayout drawerLayout,int opendedResource,int ClosedResource):base(host,drawerLayout,opendedResource,ClosedResource)
+        {
+
+            mHostActivity = host;
+            mOpenedResource = opendedResource;
+            mClosedResource = ClosedResource;
+        }
+
+        public override void OnDrawerOpened(View drawerView)
+        {
+            base.OnDrawerOpened(drawerView);
+        }
+
+        public override void OnDrawerClosed(View drawerView)
+        {
+            base.OnDrawerClosed(drawerView);
+        }
+
+        public override void OnDrawerSlide(View drawerView, float slideOffset)
+        {
+            base.OnDrawerSlide(drawerView, slideOffset);
+        }
+
+
+
+    }
+}
